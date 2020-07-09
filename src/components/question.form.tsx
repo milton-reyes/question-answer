651dea7370f2ddfbf698bed1665402d9987dd751
milton-reyes <altf4616@outlook.com>
2020-07-09 17:02:16 -0400

    added ckeditor5

--- src/components/question.form.tsx
+++ src/components/question.form.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+const ClassicEditor = require('@ckeditor/ckeditor5-build-classic')
+const CKEditor = require('@ckeditor/ckeditor5-react')
 
 const QuestionFormComponent: React.FC = () => {
+
+    
 
     return (
 
@@ -15,7 +19,26 @@ const QuestionFormComponent: React.FC = () => {
                             <input type="text" name="question" placeholder="Question?" className="form-control" required />
                         </div>
                         <div className="form-group">
-                            <textarea className="form-control" required />
+                            {/* <textarea  className="form-control" required /> */}
+                            
+                            <CKEditor
+                                editor={ClassicEditor}
+                                data="<p>Hello from CKEditor 5!</p>"
+                                onInit={(editor:any) => {
+                                    // You can store the "editor" and use when it is needed.
+                                    console.log('Editor is ready to use!', editor);
+                                }}
+                                onChange={(event:any, editor:any) => {
+                                    const data = editor.getData();
+                                    console.log({ event, editor, data });
+                                }}
+                                onBlur={(event:any, editor:any) => {
+                                    console.log('Blur.', editor);
+                                }}
+                                onFocus={(event:any, editor:any) => {
+                                    console.log('Focus.', editor);
+                                }}
+                            />
                         </div>
                         <div className="form-group">
                             <input type="submit" name="submit" className="btn btn-secondary" required />
